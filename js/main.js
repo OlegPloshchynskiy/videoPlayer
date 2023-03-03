2734fa2c3072722d9f33f4eba1e0a2bb5bd75063
Olyezha <Oleg250901@gmail.com>
2023-03-03 23:26:06 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,56 @@
+class VideoPlayer {
+  constructor(){
+      //all elements from HTML
+      this.player = document.querySelector('.player');
+      this.video = this.player.querySelector('.viewer');
+      this.progress = document.querySelector('.progress');
+      this.progressBar = this.progress.querySelector('.progress__filled');
+      this.toggle = this.player.querySelector('.toggle');
+      this.ranges = this.player.querySelectorAll('.player__slider');
+  }
+
+  //  start player
+  init(){
+      // const self = this;
+      this.events(this);
+  }
+
+  //  all events
+  events(self) {
+      this.video.addEventListener('click', (e) => this.togglePlay(e) );
+      this.toggle.addEventListener('click', (e) => this.togglePlay(e) );
+      this.ranges.forEach(range =>  range.addEventListener('input', e => {
+        this.handleRangeUpdate(e);
+        console.log(range.name);
+      }));
+      // this.ranges.forEach(range =>  range.addEventListener('mousemove', e => this.handleRangeUpdate(e) ));
+      this.video.addEventListener('timeupdate', e => this.handleProgress(e) );
+  }
+
+  //  play/pause
+  togglePlay(e) {
+      const method = this.video.paused ? 'play' : 'pause';
+      this.toggle.textContent = this.video.paused ? "▌▌" : '►';
+      this.video[method]();
+  }
+
+  //  volume and speed
+  handleRangeUpdate(e){
+
+    if (document.activeElement.name === 'volume') {
+      this.video['volume'] = e.target.value
+    } else {
+      this.video['playbackRate'] = e.target.value
+    }
+  }
+
+  //  video progress
+  handleProgress(e){
+      const percent = (this.video.currentTime / this.video.duration) * 100;
+      this.progressBar.style.flexBasis = `${percent}% `;
+  }
+}
+
+//create object video
+const video = new VideoPlayer();
+video.init();
